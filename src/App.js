1a450984cd4d2fafb2e91140b5744c5fd5bb6169
Msarmad-0099 <Sarmadidreesm@gmail.com>
2020-06-08 17:41:12 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Dinner from './dinner';
+
+function App() {
+  return (
+    <div className="App" >
+      <Dinner dishName="Chicken Khari" sweetdishName="custered"/>
+      <Dinner dishName="Zinger" sweetdishName="Russian salad"/>
+      <Dinner dishName="Nehari" sweetdishName="carrat halwa"/>
+    </div>
+  );
+}
+
+export default App;
